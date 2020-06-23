aab198e729c8340db78100bb5ea561cc408703d6
R. Garcia-Dias <rafaelagd@gmail.com>
2020-06-23 16:15:35 +0100

    Add combine_freesurfer
    A function to combine the FreeSurfer output.

--- neuroharmony/__init__.py
+++ neuroharmony/__init__.py
@@ -1,4 +1,4 @@
-from neuroharmony.data import combine_freesurfer
+from neuroharmony.data.combine_tools import combine_freesurfer
 from neuroharmony.models.harmonization import ComBat
 from neuroharmony.models.harmonization import Neuroharmony, exclude_single_subject_groups
 from neuroharmony.models.mriqc import run_mriqc
